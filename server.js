562c8ecfebab082fdc479d5fab8cdab8d9a96dfb
Nick Kotenberg <nkotenberg@fusiontms.com>
2016-09-23 20:22:40 +0000

    finised

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express(),
-    fs = require('fs'),
-    failed_response = {
-		success:0,
-		message:'Failed to read file..'
-	},
-	exec = require('child_process').exec;
+        bodyParser = require('body-parser'),
+        app = express(),
+        fs = require('fs'),
+        publicImagesUrl = '/var/www/fusion/public/images/temp/',
+        exec = require('child_process').exec
 
-function createFile(name,request,path,owner) {
-	console.log('New file coming in...');
-	var cs = request.pipe(fs.createWriteStream(path+name))
-	cs.on('finish',function() {
-		exec('chown '+owner+':'+owner+' ' + path+name + ' && chmod 777 ' + path+name,function(error,stout,stderr) {
-			if (error) {console.log(error)}
-			console.log('Finished writing',name,'to',path,'folder.')
-		})
-	})
+function createFile(request,cb) {
+        console.log('New file coming in for public consumption...',request.params.name)
+        var cs = request.pipe(fs.createWriteStream(publicImagesUrl + request.params.name))
+        cs.on('finish',function() {
+                exec('chown ubuntu:ubuntu ' + publicImagesUrl + request.params.name,function(error,stout,stderr) {
+                        if (error) {
+                                cb(error)
+                        } else {
+                                console.log('Finished writing',request.params.name,'to',publicImagesUrl,'folder.')
+                                cb(null)
+                        }
+                })
+        })
 }
 
 
-app.use(bodyParser.json({limit: '512mb'}));
-app.use(bodyParser.urlencoded({limit: '512mb', extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '512mb'}))
+app.use(bodyParser.urlencoded({limit: '512mb', extended: true}))
+app.use(bodyParser.json())
 
-app.get('/files/:name',function(req,res) {
-    res.sendFile(baseUrl+req.params.name);
-})
-
-app.post('/files/:name',function(req,res) {
-    createFile(req.params.name,req,'/var/www/files/','ubuntu');
-})
-
-app.post('/mysql/:name',function(req,res) {
-    createFile(req.params.name,req,'/tmp/','mysql')
+app.post('/public/images/:name',function(req,res) {
+        createFile(req,function(err) {
+	    res.status(200).send(null)
+        })
 })
 
 app.all('*',function(req,res) { res.redirect('https://www.fusiontms.net') })
